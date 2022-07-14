8b9d3680345c285e7c487d16859686b3f16d9a32
GnanaprakashBoopathy <gnanaprakashrocks@gmail.com>
2022-07-14 10:35:02 +0530

    updated

--- src/test/java/com/practice/JunitPractice2.java
+++ src/test/java/com/practice/JunitPractice2.java
@@ -18,5 +18,32 @@ public class JunitPractice2 {
 	public void test08() {
 		System.out.println("Test 08");
 	}
-
+	@Test
+	public void test05() {
+		System.out.println("Test 05");
+	}
+	@Test
+	public void test06() {
+		System.out.println("Test 06");
+	}@Test
+	public void test07() {
+		System.out.println("Test 07");
+	}@Test
+	public void test08() {
+		System.out.println("Test 08");
+	}
+	@Test
+	public void test05() {
+		System.out.println("Test 05");
+	}
+	@Test
+	public void test06() {
+		System.out.println("Test 06");
+	}@Test
+	public void test07() {
+		System.out.println("Test 07");
+	}@Test
+	public void test08() {
+		System.out.println("Test 08");
+	}
 }
